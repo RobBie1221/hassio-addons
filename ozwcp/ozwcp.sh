9fff10780ac487d4ff2681e300e7de5bf69ef5f3
Rob Bierbooms <mail@robbierbooms.nl>
2020-12-22 19:51:39 +0100

    Fix ozwcp.sh

--- ozwcp/ozwcp.sh
+++ ozwcp/ozwcp.sh
@@ -2,19 +2,19 @@
 
 # Link options.xml from /config/ (used by OZW within Home Assistant) to
 # the OZW used by OZWCP, so that they both will use the same options.
-ln -fs /config/options.xml /root/open-zwave/config/options.xml
+# ln -fs /config/options.xml /root/open-zwave/config/options.xml
 
 # Edit options.xml to force OZW to use /config/ for its user directory
 # (where it puts logs, scene config, network cache). Home Assistant does
 # this through other means; putting it in options.xml makes sure that OZWCP
 # will look in the same place.
-xmlstarlet ed -L -N x="http://code.google.com/p/open-zwave/"\
-	   -d '/x:Options/x:Option[@name="UserPath"]'\
-	   -s '/x:Options' -t elem -n NewOption\
-	   -i '/x:Options/NewOption' -t attr -n name -v UserPath\
-	   -i '/x:Options/NewOption' -t attr -n value -v "/config/"\
-	   -r '/x:Options/NewOption' -v Option\
-	   /config/options.xml
+# xmlstarlet ed -L -N x="http://code.google.com/p/open-zwave/"\
+# 	   -d '/x:Options/x:Option[@name="UserPath"]'\
+# 	   -s '/x:Options' -t elem -n NewOption\
+#	   -i '/x:Options/NewOption' -t attr -n name -v UserPath\
+#	   -i '/x:Options/NewOption' -t attr -n value -v "/config/"\
+#	   -r '/x:Options/NewOption' -v Option\
+#	   /config/options.xml
 
 # The part of OZW that *writes* the log respects the UserPath setting in options.xml
 # However, OZWCP's web interface expects to find it in its working directory (and its
